0288d0547fe5e798e9be63b0ab25c6a0bf308868
Boz <bekaloya@ncsu.edu>
2020-02-14 04:48:35 +0000

    Added crontab job generator and script for checking PortThreadManager status. TODO: update script to properly rerun PortThreadManager when it is down.

---
+++ honeypots/honeypot/check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+var=$(pgrep -af python | grep PortThreadManager.py | wc -l)
+
+if [ $var -gt 0 ]
+then
+	echo $(date) "PortThreadManager.py is running" >> /home/winnie/daniel-cron/check.log
+else
+	echo $(date) "PortThreadManager.py is not running" >> /home/winnie/daniel-cron/check.log
+fi
