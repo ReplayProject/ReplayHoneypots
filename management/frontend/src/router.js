17be59609f8b566f8f8ac5ee519b220bde510ba8
Seth <setherizor@gmail.com>
2020-02-18 13:55:25 -0500

    Imported Basic Frontend Project for #8

---
+++ management/frontend/src/router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import PageIndex from './pages/index.vue'
+import PageAbout from './pages/about.vue'
+import PageEdit from './pages/edit.vue'
+import PageOverview from './pages/overview.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: PageIndex
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: PageAbout
+    },
+    {
+      path: '/edit',
+      name: 'edit',
+      component: PageEdit
+    },
+    {
+      path: '/overview',
+      name: 'overview',
+      component: PageOverview
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
