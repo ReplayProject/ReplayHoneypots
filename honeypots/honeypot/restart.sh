926bf843c4260757125944ae017a37923791c183
Boz <bekaloya@ncsu.edu>
2020-02-27 05:22:08 +0000

    Removed CronInstaller from PortThreadManager. CronInstaller can now be called directly. Moved Cron's logs into the logs folder. - @dtpham2

--- honeypots/honeypot/restart.sh
+++ honeypots/honeypot/restart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-var=$(pgrep -af python | grep PortThreadManager.py | wc -l)
+var=$(pgrep -af PortThreadManager.py | wc -l)
 
 if [ $var -gt 0 ]
 then
-	echo $(date) 'PortThreadManager.py is running.' >> /home/winnie/check.log
+	echo $(date) 'PortThreadManager.py is running.' >> /home/winnie/2020SpringTeam18/honeypots/logs/cron.txt
 else
-	echo $(date) 'Running: python3 /home/yogi/2020SpringTeam18/honeypots/honeypot/PortThreadManager.py -c /home/yogi/2020SpringTeam18/honeypots/config/new-config.json.' >> /home/winnie/check.log
-	cd /home/yogi/2020SpringTeam18/honeypots && pip3 install -r requirements.txt
-	cd /home/yogi/2020SpringTeam18/honeypots/honeypot && python3 /home/yogi/2020SpringTeam18/honeypots/honeypot/PortThreadManager.py -c /home/yogi/2020SpringTeam18/honeypots/config/new-config.json
+	echo $(date) 'Running: python3 /home/winnie/2020SpringTeam18/honeypots/honeypot/PortThreadManager.py -c /home/winnie/2020SpringTeam18/honeypots/config/new-config.json.' >> /home/winnie/2020SpringTeam18/honeypots/logs/cron.txt
+	cd /home/winnie/2020SpringTeam18/honeypots && pip3 install -r requirements.txt
+	cd /home/winnie/2020SpringTeam18/honeypots/honeypot && python3 /home/winnie/2020SpringTeam18/honeypots/honeypot/PortThreadManager.py -c /home/winnie/2020SpringTeam18/honeypots/config/new-config.json
 fi
