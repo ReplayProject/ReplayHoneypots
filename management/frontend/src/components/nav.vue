ccbd9ed4cd68ba2c93b53c468f9a26744cd3b233
Seth <setherizor@gmail.com>
2020-03-17 16:32:03 -0400

    Removed query page and formatted package.json

--- management/frontend/src/components/nav.vue
+++ management/frontend/src/components/nav.vue
@@ -27,11 +27,6 @@
           >Terminal</router-link
         >
       </li>
-      <li class="mb2">
-        <router-link to="/query" class="block link dim blue"
-          >Query Panel</router-link
-        >
-      </li>
     </ul>
     <!-- More Realistic Options -->
     <h2 class="ttu mt0 mb2 f6 fw5 silver">Dashboards</h2>
