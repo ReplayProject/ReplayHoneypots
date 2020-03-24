dad559ac8f0fed81957779dbf25d5832ae843c35
rmzingle <rmzingle@ncsu.edu>
2020-03-24 11:08:42 -0400

    Updated configurable deployment script #13

--- dep/deploy.sh
+++ dep/deploy.sh
@@ -2,11 +2,11 @@
 
 source config.txt
 
-sudo scp -i $KEY $REPOPATH $REMOTE:$REMOTEPATH
-echo pass | sudo ssh -i $KEY -tt $REMOTE << EOF
+sudo scp -i /shared/manager_rsa $REPO $REMOTENAME@$REMOTEIP:/home/$REMOTENAME
+echo pass | sudo ssh -i $KEYPATH -tt $REMOTENAME@$REMOTEIP << EOF
 mkdir repo_test
-mv $REMOTEPATH/$REPO $REMOTEPATH/repo_test/$REPO
-tar -C $REMOTEPATH/repo_test -xf $REMOTEPATH/repo_test/$REPO
-rm $REMOTEPATH/repo_test/$REPO
-cd $CRONPATH; sudo python3 CronInstaller.py -p PortThreadManager.py -c ../config/new-config.json
+mv /home/$REMOTENAME/repo.tar.gz /home/$REMOTENAME/repo_test/repo.tar.gz
+tar -C /home/$REMOTENAME/repo_test -xf /home/$REMOTENAME/repo_test/repo.tar.gz
+rm /home/$REMOTENAME/repo_test/repo.tar.gz
+cd /home/$REMOTENAME/helloworld/2020SpringTeam18/honeypots/honeypot; sudo python3 CronInstaller.py -p PortThreadManager.py -c ../config/new-config.json
 $REMOTEPASS
