daafd9cc11815ddb8b3e3dd40d1212e22f5ed495
Seth Parrish <shparri2@ncsu.edu>
2020-03-25 22:27:29 -0400

    Renamed Function and fixed ptm (Activate)

--- honeypots/honeypot/PTMTests.py
+++ honeypots/honeypot/PTMTests.py
@@ -6,7 +6,7 @@ import time
 class TestRedeploy(unittest.TestCase):
     def test_redeploy(self):
         man = PortThreadManager()
-        man.deploy(r'../config/pt.cfg')
+        man.activate(r'../config/pt.cfg')
         #Need time to make sure everything initially deploys
         time.sleep(5)
 
@@ -14,19 +14,20 @@ class TestRedeploy(unittest.TestCase):
         self.assertTrue(len(man.responseData.keys()) == 13)
         self.assertTrue("49667" in man.responseData.keys())
         self.assertTrue(man.processList["49667"].port == "49667" and man.processList["49667"].isRunning == True)
-        
+
         self.assertTrue(len(man.snifferThread.openPorts) == 13)
         self.assertTrue("192.2.2.1" in man.snifferThread.whitelist)
         self.assertTrue(man.snifferThread.honeypotIP == "192.168.42.51")
 
         #Redeploying
-        man.deploy(r'../config/pt_altered.cfg', updateSniffer=True, updateOpenPorts=True)
+        man.activate(r'../config/pt_altered.cfg',
+                     updateSniffer=True, updateOpenPorts=True)
 
         self.assertTrue(len(man.responseData.keys()) == 7)
         self.assertFalse("49667" in man.responseData.keys())
         self.assertTrue("430" in man.responseData.keys())
         self.assertTrue(man.processList["430"].port == "430" and man.processList["430"].isRunning == True)
-        
+
         self.assertTrue(len(man.snifferThread.openPorts) == 7)
         self.assertTrue("5.6.7.8" in man.snifferThread.whitelist)
         self.assertTrue(man.snifferThread.honeypotIP == "192.168.42.55")
