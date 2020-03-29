ded6025fe4aab754e529a7c1b1c327cb27d7efb3
Seth Parrish <shparri2@sd-vm06.csc.ncsu.edu>
2020-03-29 02:54:54 -0400

    Refactor pouchdb -> couchdb for #18 & config prune

--- service_deploy.sh
+++ service_deploy.sh
@@ -5,7 +5,11 @@ docker service create \
    --replicas 1 \
    --with-registry-auth \
    --network host \
+   --env DB_URL="http://admin:couchdb@192.168.23.50:5984" \
    --name replay-honeypot \
    --constraint node.role==worker \
    --placement-pref spread=node.id \
    cloud.canister.io:5000/seth/replay-honeypot:latest
+
+# When we want to test replication and not direct writing
+# TARGET_ADDR='http://admin:couchdb@couchdb:5984'
