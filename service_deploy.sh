63c19b85a72740f529b2f1e569ca44d7e5374059
Seth Parrish <shparri2@sd-vm06.csc.ncsu.edu>
2020-03-30 21:03:58 -0400

    Added option to disable replication at deployment

--- service_deploy.sh
+++ service_deploy.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-  #  --mount dst=/usr/src/frontend/.data \
 docker service create \
    --replicas 1 \
    --with-registry-auth \
    --network host \
-   --env DB_URL="http://honeypots:securehoneypassword@192.168.23.50:5984" \
+   --env DB_URL="" \
    --name replay-honeypot \
    --constraint node.role==worker \
    --placement-pref spread=node.id \
    cloud.canister.io:5000/seth/replay-honeypot:latest
 
-# When we want to test replication and not direct writing
-# TARGET_ADDR='http://admin:couchdb@couchdb:5984'
+
+# Optional configurations
+#  --mount dst=/usr/src/frontend/.data \
+#  --env DB_URL="http://honeypots:securehoneypassword@192.168.23.50:5984" \
+#  --env TARGET_ADDR="http://honeypots:securehoneypassword@192.168.23.50:5984" \
