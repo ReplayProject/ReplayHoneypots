237aeabf81008fb08bda87ce1676df7f729d5480
Seth Parrish <shparri2@sd-vm06.csc.ncsu.edu>
2020-04-03 14:50:42 -0400

    Updates for scripts with config settings #12

---
+++ create_configs.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Script to create docker configs for properties.cfg and senddata.json
+
+# Set versioning tag
+TAG=$(date +%s)
+export TAG=$TAG
+echo "Set TAG to: $TAG"
+
+# Create config
+docker config create honey-cfg-$TAG $PWD/config/honeypot.cfg > /dev/null
+docker config create honey-data-$TAG $PWD/config/senddata.json > /dev/null
+
+# List existing configs
+echo -e "\n"
+docker config ls
+
+# Do this so script should be sourced (to export TAG)
+return $TAG
--- service_deploy.sh
+++ service_deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "Deploying Honeypots with config revision: $TAG"
+
 docker service create \
    --with-registry-auth \
    --env DB_URL="http://honeypots:securehoneypassword@192.168.23.50:5984" \
@@ -10,12 +12,13 @@ docker service create \
    --restart-condition on-failure \
    --network host \
    --name replay-honeypot \
+   --config src="honey-cfg-$TAG",target="/usr/src/app/honeypot/config/properties.cfg"\
+   --config src="honey-data-$TAG",target="/usr/src/app/honeypot/data/senddata.json"\
    cloud.canister.io:5000/seth/replay-honeypot:latest
 
 
 # Optional configurations
 #  --mount type=bind,src=/shared/2020SpringTeam18/honeypots/honeypot/Databaser.py,dst=/usr/src/app/honeypot/Databaser.py \
 #  --mount type=bind,src=/shared/2020SpringTeam18/honeypots/honeypot/Sniffer.py,dst=/usr/src/app/honeypot/Sniffer.py \
-#  --env DB_URL="" \
 #  --mount dst=/usr/src/frontend/.data \
 #  --env TARGET_ADDR="http://honeypots:securehoneypassword@192.168.23.50:5984" \
