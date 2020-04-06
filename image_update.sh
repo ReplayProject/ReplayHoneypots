58d106c36a8ef3204e1ae01acc55f688f73b8937
Seth Parrish <shparri2@sd-vm06.csc.ncsu.edu>
2020-04-06 01:06:16 -0400

    switched from external registry to throwaway #15

--- image_update.sh
+++ image_update.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker service update replay-honeypot --image cloud.canister.io:5000/seth/replay-honeypot:latest --force --with-registry-auth
+docker service update replay-honeypot --image 127.0.0.1:5000/seth/replay-honeypot:latest --force --with-registry-auth
