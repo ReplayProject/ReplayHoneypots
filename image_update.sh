3c0109abae01dfc0004cb5282fa1cd14febbb39b
Seth Parrish <shparri2@sd-vm06.csc.ncsu.edu>
2020-04-09 12:09:01 -0400

    Formatting for docker scripts

--- image_update.sh
+++ image_update.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 docker service update replay-honeypot --image 127.0.0.1:5000/seth/replay-honeypot:latest --force --with-registry-auth
+docker service update honey_replay-manager --image 127.0.0.1:5000/seth/replay-manager:latest --force --with-registry-auth
