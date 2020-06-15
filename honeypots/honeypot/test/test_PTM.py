0cd53d4bb18f0a62adf439e234fa78139bf6432e
Seth <setherizor@gmail.com>
2020-06-15 21:13:08 +0000

    redid PTM tests & made isRunning a listener flag

--- honeypots/honeypot/test/test_PTM.py
+++ honeypots/honeypot/test/test_PTM.py
@@ -1,60 +1,79 @@
-#
-# TODO: REVISIT when move to TRIO is done
-#
-
 from PortThreadManager import PortThreadManager
-import unittest
+import trio
+from functools import partial
 import os
-import time
 
-class TestRedeploy(unittest.TestCase):
-    def test_redeploy(self):
+
+class TestRedeploy:
+    async def test_redeploy(self, nursery):
         man = PortThreadManager()
-        man.activate(r'../testing_configs/pt.cfg')
-        #Need time to make sure everything initially deploys
-        time.sleep(5)
 
-        #Socket testing with senddata.json
-        self.assertTrue(len(man.responseData.keys()) == 13)
-        self.assertTrue("49667" in man.responseData.keys())
-        self.assertTrue(man.processList["49667"].port == "49667" and man.processList["49667"].isRunning == True)
+        man_ctx = await nursery.start(
+            partial(man.activate, r"../testing_configs/pt.cfg")
+        )
 
-        self.assertTrue(len(man.snifferThread.openPorts) == 13)
-        self.assertTrue("192.2.2.1" in man.snifferThread.whitelist)
-        self.assertTrue(man.snifferThread.honeypotIP == "192.168.42.51")
-        self.assertTrue(man.snifferThread.portWhitelist[1] == 9000)
+        # Need time to make sure everything initially deploys
+        await trio.sleep(5)
 
-        #Redeploying
-        x = man.activate(r'../testing_configs/pt_altered.cfg',
-                     updateSniffer=True, updateOpenPorts=True)
+        # Socket testing with senddata.json
+        assert len(man.responseData.keys()) == 13
+        assert "49667" in man.responseData.keys()
 
-        #changed both, should return a 3
-        self.assertTrue(x == 3)
+        assert (
+            man.processList["49667"].port == "49667"
+            and man.processList["49667"].isRunning == True
+        )
 
-        self.assertTrue(len(man.responseData.keys()) == 7)
-        self.assertFalse("49667" in man.responseData.keys())
-        self.assertTrue("430" in man.responseData.keys())
-        self.assertTrue(man.processList["430"].port == "430" and man.processList["430"].isRunning == True)
+        assert len(man.sniffer.openPorts) == 13
+        assert "192.2.2.1" in man.sniffer.whitelist
+        assert man.sniffer.honeypotIP == "192.168.42.51"
+        assert man.sniffer.portWhitelist[1] == 9000
 
-        self.assertTrue(len(man.snifferThread.openPorts) == 7)
-        self.assertTrue("5.6.7.8" in man.snifferThread.whitelist)
-        self.assertTrue(man.snifferThread.honeypotIP == "192.168.42.55")
-        self.assertTrue(man.snifferThread.portWhitelist[1] == 8000)
+        # Cleaning
+        man_ctx.cancel()  # clean tasks
+        man.processList = dict()  # clean listener objects
 
-        x = man.activate(r'../testing_configs/pt.cfg',
-                     updateSniffer=False, updateOpenPorts=True)
-        self.assertTrue(x == 2)
+        # Redeploying
+        man_ctx = await nursery.start(
+            partial(
+                man.activate,
+                r"../testing_configs/pt_altered.cfg",
+                updateSniffer=True,
+                updateOpenPorts=True,
+            )
+        )
 
-        x = man.activate(r'../testing_configs/pt.cfg',
-                     updateSniffer=True, updateOpenPorts=False)
-        self.assertTrue(x == 1)
+        await trio.sleep(2)
 
-        #We tell it to update sniffer and open ports, but nothing changes. Should return 0.
-        x = man.activate(r'../testing_configs/pt.cfg',
-                     updateSniffer=True, updateOpenPorts=True)
-        self.assertTrue(x == 0)
+        assert len(man.responseData.keys()) == 7
+        assert not ("49667" in man.responseData.keys())
+        assert "430" in man.responseData.keys()
+        assert (
+            man.processList["430"].port == "430"
+            and man.processList["430"].isRunning == True
+        )
 
+        assert len(man.sniffer.openPorts) == 7
+        assert "5.6.7.8" in man.sniffer.whitelist
+        assert man.sniffer.honeypotIP == "192.168.42.55"
+        assert man.sniffer.portWhitelist[1] == 8000
 
+        man_ctx.cancel()  # clean tasks
+        man.processList = dict()  # clean listener objects
 
-if __name__ == '__main__':
-    unittest.main()
+        # TODO: test PTM reconfiguration (if still supported)
+        # x = man.activate(
+        #     r"../testing_configs/pt.cfg", updateSniffer=False, updateOpenPorts=True
+        # )
+        # assert x == 2
+
+        # x = man.activate(
+        #     r"../testing_configs/pt.cfg", updateSniffer=True, updateOpenPorts=False
+        # )
+        # assert x == 1
+
+        # # We tell it to update sniffer and open ports, but nothing changes. Should return 0.
+        # x = man.activate(
+        #     r"../testing_configs/pt.cfg", updateSniffer=True, updateOpenPorts=True
+        # )
+        # assert x == 0
